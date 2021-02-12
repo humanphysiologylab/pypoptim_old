3a47a7fdb201759494815d214a53f6e5c322f131
--local <pikunov@phystech.edu>
2021-02-12 22:51:43 +0300

    fluo-3 koivumaki

--- src/model_ctypes/_koivumaki/koivumaki.h
+++ src/model_ctypes/_koivumaki/koivumaki.h
@@ -1,11 +1,11 @@
 #ifndef _KOIVUMAKI_H_
 #define _KOIVUMAKI_H_
 
-#define S_SIZE 43
-#define C_SIZE 89
+#define S_SIZE 48
+#define C_SIZE 92
 #define A_SIZE 107
 
-void initialize_states_default(double *STATES);
+void initialize_states_default(double *STATES, const double *CONSTANTS);
 
 void initialize_constants_default(double *CONSTANTS);
 
