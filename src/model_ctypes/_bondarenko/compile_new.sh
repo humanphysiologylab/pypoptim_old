7ab46794f135cac0ce76796514076e8c93b5c88c
Andrey Pikunov <pikunov@phystech.edu>
2021-03-04 19:47:43 +0300

    tidy bondarenko

--- src/model_ctypes/_bondarenko/compile_new.sh
+++
@@ -1,4 +0,0 @@
-rm ./bondarenko.o
-rm ./bondarenko.so
-gcc  -Wall -std=c99 -O3 -ffast-math -march=native -DCFODE_STATIC -fPIC -DNDEBUG -c bondarenko_control.c -o bondarenko.o
-gcc -shared -o bondarenko.so bondarenko.o
