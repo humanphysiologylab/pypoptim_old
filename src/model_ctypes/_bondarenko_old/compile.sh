6a75a324a3f16800aebaec879c0446b1dea2ce97
drybashlykov <drybashlykov@gmail.com>
2021-06-03 18:07:25 +0300

    removed trash

--- src/model_ctypes/_bondarenko_old/compile.sh
+++
@@ -1,5 +0,0 @@
-rm ./*.o
-rm ./*.so
-gcc  -Wall -std=c99 -O3 -ffast-math -march=native -DCFODE_STATIC -fPIC -DNDEBUG -c bondarenko_control.c -o bondarenko.o
-# gcc  -Wall -std=c99 -O3 -ffast-math -march=native -DCFODE_STATIC -fPIC -DNDEBUG -c model.c -o model.o
-gcc -shared -o bondarenko.so bondarenko.o # model.o
