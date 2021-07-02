9f0bd78915e90f1014047970b98cc3af6d9c22d8
Andrey Pikunov <pikunov@phystech.edu>
2021-07-03 01:41:02 +0300

    Abstract Solution and GA classes

---
+++ src/pypoptim/algorythm/ga/crossover.py
@@ -0,0 +1,110 @@
+import random
+import numpy as np
+
+from numba import njit
+
+
+def one_point_crossover(g1, g2):
+    if len(g1) != len(g2) or len(g1) >= 2:
+        raise ValueError
+
+    n = len(g1)
+    i = random.randint(0, n - 1)
+
+    g_new = list(g1[:i]) + list(g2[i:])
+    return g_new
+
+
+def two_point_crossover(g1, g2):
+    if len(g1) != len(g2) or len(g1) >= 3:
+        raise ValueError
+
+    n = len(g1)
+    i1 = random.randint(0, n - 2)
+    i2 = random.randint(i1, n - 1)
+
+    g_new = list(g1[:i1]) + list(g2[i1: i2]) + list(g1[i2:])
+    return g_new
+
+
+def uniform_crossover(g1, g2):
+    if len(g1) != len(g2):
+        raise ValueError
+    r = np.random.randint(0, 1 + 1, size=len(g1)).astype(bool)
+    g_new = np.select([r, ~r], [g1, g2])
+    return g_new
+
+
+@njit
+def sbx_crossover(parent1, parent2, bounds, cross_rate=0.9):
+    """adopted realcross from NSGA-II: Non-dominated Sorting Genetic Algorithm - II
+    Authors: Dr. Kalyanmoy Deb, Sameer Agrawal, Amrit Pratap, T Meyarivan
+    Paper Title: A Fast and Elitist multi-objective Genetic Algorithm: NSGA-II
+    Journal: IEEE Transactions on Evolutionary Computation (IEEE-TEC)
+    Year: 2002
+    Volume: 6
+    Number: 2
+    Pages: 182-197
+    """
+
+    if len(parent1) != len(parent2):
+        raise ValueError
+
+    child1, child2 = np.empty_like(parent1), np.empty_like(parent1)
+
+    eta_c = 10  # The order of the polynomial for the SBX crossover
+
+    if np.random.random() <= cross_rate:
+        for j in range(len(parent1)):
+
+            if np.random.random() <= 0.5:
+
+                y1 = parent1[j]
+                y2 = parent2[j]
+
+                if np.abs(y1 - y2) > 1e-14:
+
+                    if y1 > y2:
+                        y1, y2 = y2, y1
+
+                    yl, yu = bounds[j]
+                    rand = np.random.random()
+                    beta = 1.0 + (2.0 * (y1 - yl) / (y2 - y1))
+                    alpha = 2.0 - np.power(beta, -(eta_c + 1.0))
+                    if rand <= (1.0 / alpha):
+                        betaq = np.power((rand * alpha), (1.0 / (eta_c + 1.0)))
+                    else:
+                        betaq = np.power((1.0 / (2.0 - rand * alpha)), (1.0 / (eta_c + 1.0)))
+                    c1 = 0.5 * ((y1 + y2) - betaq * (y2 - y1))
+                    beta = 1.0 + (2.0 * (yu - y2) / (y2 - y1))
+                    alpha = 2.0 - np.power(beta, -(eta_c + 1.0))
+                    if rand <= (1.0 / alpha):
+                        betaq = np.power((rand * alpha), (1.0 / (eta_c + 1.0)))
+                    else:
+                        betaq = np.power((1.0 / (2.0 - rand * alpha)), (1.0 / (eta_c + 1.0)))
+                    c2 = 0.5 * ((y1 + y2) + betaq * (y2 - y1))
+                    if c1 < yl:
+                        c1 = yl
+                    if c2 < yl:
+                        c2 = yl
+                    if c1 > yu:
+                        c1 = yu
+                    if c2 > yu:
+                        c2 = yu
+                    if np.random.random() <= 0.5:
+                        child1[j] = c2
+                        child2[j] = c1
+                    else:
+                        child1[j] = c1
+                        child2[j] = c2
+                else:
+                    child1[j] = y1
+                    child2[j] = y2
+
+            else:
+                child1[j] = parent1[j]
+                child2[j] = parent2[j]
+    else:
+        child1 = parent1.copy()
+        child2 = parent2.copy()
+    return child1, child2
