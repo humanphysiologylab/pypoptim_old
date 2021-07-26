a78d9fb0b984be8d63fcf1e34abfe4908dcc2c14
Andrey Pikunov <pikunov@phystech.edu>
2021-07-26 16:54:28 +0300

    fix tests

--- src/pypoptim/algorythm/ga/selection.py
+++ src/pypoptim/algorythm/ga/selection.py
@@ -4,4 +4,4 @@ import numpy as np
 def tournament_selection(p, k=2, rng=None):
     if rng is None:
         rng = np.random.default_rng()
-    return rng.choice(p, k)
+    return min(rng.choice(p, k, replace=False))
--- src/pypoptim/tests/test_ga.py
+++ src/pypoptim/tests/test_ga.py
@@ -5,9 +5,9 @@ from ..algorythm.ga.selection import tournament_selection
 
 def test_tournament_selection():
     p = [1, 0]
-    assert tournament_selection(p) is p[1]
+    assert tournament_selection(p) == p[1]
     p.append(-1)
-    assert tournament_selection(p, k=3) is p[-1]
+    assert tournament_selection(p, k=3) == p[-1]
 
 #
 # def test_do_step():
